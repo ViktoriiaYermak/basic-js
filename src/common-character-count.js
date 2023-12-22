fd69465ed19173d72720a3790ddc618b5377d3c4
ViktoriiaYermak <yermakvictoriia@gmail.com>
2023-12-22 20:46:42 +0200

    feat: completed tasks carbon-dating, common-character-count, dns-stats, simple-chain, sort-by-height, (Fri, Dec 22, 2023 08:46:42 PM)

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const s = s2.split("");
+  let commonCount = 0;
+
+  for (let i = 0; i < s1.length; i++) {
+    const char = s1[i];
+    const index = s.indexOf(char);
+
+    if (index !== -1) {
+      s[index] = null;
+      commonCount += 1;
+    }
+  }
+
+  return commonCount;
 }
 
 module.exports = {
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+
+  for (const domain of domains) {
+    const segments = domain.split('.').reverse();
+    let key = '';
+    for (const segment of segments) {
+      key += `.${segment}`;
+      result[key] = (result[key] || 0) + 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
